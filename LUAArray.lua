31c1c295920990d6d7eaf819e9ee7f6f713d6b7d
Luca Mauri <lucamauri@users.noreply.github.com>
2025-11-01 18:18:39 +0100

    CSV updated and LUA added

---
+++ LUAArray.lua
@@ -0,0 +1,17 @@
+local Titles = {
+		wikitrek = 'WikiTrek',
+		datatrek = 'DataTrek',
+		enma = 'Memory Alpha (inglese)',
+		itma = 'Memory Alpha (italiano)',
+		enmb = 'Memory Beta (inglese)',
+		sto = 'Star Trek Online wiki',
+		wikidata = 'Pagina della entità su Wikidata',
+		enwiki = 'Wikipedia (inglese)',
+		itwiki = 'Wikipedia (italiano)',
+		dewiki = 'Wikipedia (tedesco)',
+		dema = 'Memory Alpha (tedesco)',
+		demb = 'Memory Beta (tedesco)',
+		fanlore = 'Fanlore',
+		trekipedia = 'Trekipedia',
+		commonswiki = 'Wikimedia Commons'
+	}
